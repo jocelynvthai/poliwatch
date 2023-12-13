ba55beac106c0e43cebd0b40934d0e51f3ee3fc9
jocelynvthai <jocelynvthai@berkeley.edu>
2023-12-12 18:57:04 -0800

    testing cache

--- src/book/01.py
+++ src/book/01.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from helper import magic
 
-magic.pause(1)
-st.markdown(f"<h1 style='font-size: 25px;'>Problem Statement</h1>", unsafe_allow_html=True)
-magic.pause(2)
-st.markdown(f"<h1 style='font-size: 20px;'>Congress has an insider trading problem. Can the American people be sure that congressional members are acting in the best interest of the public over private investment portfolios? </h1>", unsafe_allow_html=True)
+@st.cache_data
+def main():
+    magic.pause(1)
+    st.markdown(f"<h1 style='font-size: 25px;'>Problem Statement</h1>", unsafe_allow_html=True)
+    magic.pause(2)
+    st.markdown(f"<h1 style='font-size: 20px;'>Congress has an insider trading problem. Can the American people be sure that congressional members are acting in the best interest of the public over private investment portfolios? </h1>", unsafe_allow_html=True)
+
+main()
