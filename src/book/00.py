05cbbe4a16ab886ecbb5933adcb381f806d54968
jocelynvthai <jocelynvthai@berkeley.edu>
2023-12-12 18:33:02 -0800

    00

--- src/book/00.py
+++ src/book/00.py
@@ -1,15 +1,19 @@
 import streamlit as st
 from helper import magic
 
+if 'start' not in st.session_state:
+    st.session_state['start'] = False
+
 
 start = st.button("Let's get started")
 
 if start:
-    magic.pause(1)
+    st.session_state['start'] = True
     st.markdown(f"<h1 style='font-size: 25px;'>Welcome to...</h1>", unsafe_allow_html=True)
     magic.pause(2)
     st.markdown(f"<h1 style='font-size: 20px;'>Identifying Conflicts of Interest in the Securities Transactions of U.S. Congressional Members</h1>", unsafe_allow_html=True)
-
+    magic.pause(2)
+    
     problem = st.button("Problem Statement")
-    if problem: 
+    if problem and st.session_state['start']: 
         st.markdown(f"<h1 style='font-size: 20px;'>Congress has an insider trading problem. Can the American people be sure that congressional members are acting in the best interest of the public over private investment portfolios? </h1>", unsafe_allow_html=True)    
