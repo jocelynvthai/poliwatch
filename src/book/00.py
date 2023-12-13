43ba43738b64cee019ef7d2a97830e4d59b025c8
jocelynvthai <jocelynvthai@berkeley.edu>
2023-12-12 18:31:31 -0800

    00

--- src/book/00.py
+++ src/book/00.py
@@ -9,3 +9,7 @@ if start:
     st.markdown(f"<h1 style='font-size: 25px;'>Welcome to...</h1>", unsafe_allow_html=True)
     magic.pause(2)
     st.markdown(f"<h1 style='font-size: 20px;'>Identifying Conflicts of Interest in the Securities Transactions of U.S. Congressional Members</h1>", unsafe_allow_html=True)
+
+    problem = st.button("Problem Statement")
+    if problem: 
+        st.markdown(f"<h1 style='font-size: 20px;'>Congress has an insider trading problem. Can the American people be sure that congressional members are acting in the best interest of the public over private investment portfolios? </h1>", unsafe_allow_html=True)    
