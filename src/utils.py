459570abcca8f35eb2f828b632803474c51e1816
jocelynvthai <jocelynvthai@berkeley.edu>
2023-12-12 19:59:39 -0800

    db

--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,8 @@ def set_db_connection():
     s3 = boto3.resource(
         service_name='s3',
         region_name='us-east-1',
-        aws_access_key_id='AKIA2JYPH33Q4KP3JE3Y',
-        aws_secret_access_key='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'
+        aws_access_key_id='AKIAUBVWHVMYZ6DB3MWH',
+        aws_secret_access_key='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'
     )
 
     return s3
