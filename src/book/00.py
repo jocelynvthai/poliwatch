c7c8981e1e1db401c5ad01965a0d3b6cd6dfd17f
jocelynvthai <jocelynvthai@berkeley.edu>
2023-12-12 18:14:27 -0800

    00

--- src/book/00.py
+++ src/book/00.py
@@ -6,7 +6,7 @@ if "do_matrix_effect" not in st.session_state:
     st.session_state["do_matrix_effect"] = False
 
 if not st.session_state.do_matrix_effect:
-    magic.pause(1)
+    magic.pause(2)
     st.header("Welcome to...")
-    magic.pause(1)
+    magic.pause(3)
     st.subheader("Identifying Conflicts of Interest in the Securities Transactions of U.S. Congressional Members")  
